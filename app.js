7440f3aa895e0497559bcadfd81d29097bbf41de
CodeAGenie <nwibeg@gmail.com>
2023-07-19 13:56:23 +0100

    revision

--- app.js
+++ app.js
@@ -8,4 +8,19 @@
 // })('brad')
 
 // Property methods
+// const todo = {
+//     add: function(){
+//         console.log('add todo');
+//     },
+//     edit:function(){
+//         console.log('edit todo');
+//     }
+// }
+// todo.delete =function(){
+//     console.log('delete todo...')
+// }
+// todo.add();
+// todo.edit();
+// todo.delete();
 
+// to loop through an array
